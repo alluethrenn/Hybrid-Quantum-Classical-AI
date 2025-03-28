b93446bdd3f918a8c686b748e17f640432f43e74
alluethrenn <amorleinis89@gmail.com>
2025-03-27 21:41:40 -0500

    Update datasets.py

--- src/deep_learning/datasets.py
+++ src/deep_learning/datasets.py
@@ -1,1 +1,20 @@
+# src/deep_learning/datasets.py
 
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_mnist_dataloader(batch_size=32):
+    """Loads the MNIST dataset and returns DataLoader objects."""
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
