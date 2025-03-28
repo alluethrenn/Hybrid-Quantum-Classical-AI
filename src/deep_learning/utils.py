170a4977c9cdacbebadc139ffeaeeb9d42ff9723
alluethrenn <amorleinis89@gmail.com>
2025-03-27 21:43:41 -0500

    Update utils.py

--- src/deep_learning/utils.py
+++ src/deep_learning/utils.py
@@ -1,1 +1,11 @@
+# src/deep_learning/utils.py
 
+import torch
+from model import DeepLearningModel
+
+def load_trained_model(model_path="results/deep_learning_model.pth"):
+    """Loads a trained deep learning model."""
+    model = DeepLearningModel()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
